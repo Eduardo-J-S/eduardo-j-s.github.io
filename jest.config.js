6474fabd1fb72382760572510b30e923a9791285
Eduardo-J-S <eduardojose59@hotmail.com>
2024-01-19 04:54:57 -0300

    add: changes

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-module.exports = {
-    testEnvironment: process.env.REACT_APP_CUSTOM_OPTION ? 'your-custom-environment' : 'jsdom',
-    // outras configurações do Jest...
-  };
-  
