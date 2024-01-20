639f8df5e1c1bad55b41599e53640bb532c026b9
Eduardo-J-S <eduardojose59@hotmail.com>
2024-01-19 22:47:29 -0300

    add particles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import '@tsparticles/react';
+import '@tsparticles/slim';
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from '@tsparticles/slim';
 import Home from './containers/home';
 import About from './containers/about';
 import Resume from './containers/resume';
@@ -7,12 +12,34 @@ import Skills from './containers/skills';
 import Portfolio from './containers/portfolio';
 import Contact from './containers/contact';
 import Navbar from './components/navBar';
-
+import particles from './utils.js/particles';
 
 function App() {
+  const [init, setInit] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const renderParticlesJsInHome = location.pathname === "/";
+
+  let particlesObj = null;
+
+  if (init) {
+    particlesObj = renderParticlesJsInHome ? <Particles id="particles" options={particles} /> : '';
+  }
+
+
 
   return (
     <div className="App">
+      {/* particles js */}
+      {particlesObj}
       {/* navbar */}
       <Navbar />
       {/* main page content*/}
